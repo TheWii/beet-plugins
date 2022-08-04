413bb407262401a503c31e76205d5fda55c98f8e
TheWii <67249660+TheWii@users.noreply.github.com>
2022-08-04 14:57:22 -0300

    feat: add root_relative_location.py

---
+++ beet_plugins/root_relative_location.py
@@ -0,0 +1,78 @@
+"""Plugin that resolves root-relative resource locations."""
+
+
+__all__ = [
+    "RootRelativeLocationParser",
+    "RootRelativeLocationLiteralParser",
+    "resolve_using_database",
+]
+
+
+from dataclasses import dataclass
+
+from beet import Context
+from bolt import AstValue
+from tokenstream import TokenStream, set_location
+from mecha import (
+    AstResourceLocation,
+    CommentDisambiguation,
+    Mecha,
+    Parser,
+)
+
+
+PATTERN = r"#?~[0-9a-z_./-]+"
+
+
+def beet_default(ctx: Context):
+    mc = ctx.inject(Mecha)
+    parsers = mc.spec.parsers
+    parsers["resource_location_or_tag"] = CommentDisambiguation(
+        RootRelativeLocationParser(parser=parsers["resource_location_or_tag"], ctx=ctx)
+    )
+    parsers["bolt:literal"] = RootRelativeLocationLiteralParser(
+        parser=parsers["bolt:literal"], ctx=ctx
+    )
+
+
+@dataclass
+class RootRelativeLocationParser:
+    """Parser that resolves root-relative resource locations."""
+
+    parser: Parser
+    ctx: Context
+
+    def __call__(self, stream: TokenStream) -> AstResourceLocation:
+        with stream.syntax(root_resource_location=PATTERN):
+            token = stream.get("root_resource_location")
+
+            if token is None:
+                return self.parser(stream)
+
+            is_tag = token.value.startswith("#")
+            value = token.value[2:] if is_tag else token.value[1:]
+            full_path = self.ctx.generate.path(value)
+            namespace, _, path = full_path.rpartition(":")
+
+            node = AstResourceLocation(is_tag=is_tag, namespace=namespace, path=path)
+            return set_location(node, token)
+
+
+@dataclass
+class RootRelativeLocationLiteralParser:
+    parser: Parser
+    ctx: Context
+
+    def __call__(self, stream: TokenStream) -> AstValue:
+        with stream.syntax(root_resource_location=PATTERN):
+            token = stream.get("root_resource_location")
+
+            if token is None:
+                return self.parser(stream)
+
+            is_tag = token.value.startswith("#")
+            stripped = token.value[2:] if is_tag else token.value[1:]
+            value = "#" * is_tag + self.ctx.generate.path(stripped)
+
+            node = AstValue(value=value)
+            return set_location(node, token)
