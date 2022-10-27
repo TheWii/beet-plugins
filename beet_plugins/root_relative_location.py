97b2701463595f891c2881303310afc19b06cd2b
TheWii <67249660+TheWii@users.noreply.github.com>
2022-10-27 09:09:25 -0300

    feat: root relative locations evaluate at runtime

--- beet_plugins/root_relative_location.py
+++ beet_plugins/root_relative_location.py
@@ -2,22 +2,29 @@
 
 
 __all__ = [
+    "AstRootResourceLocation",
+    "beet_default",
+    "resolve_root_resource_location",
+    "root_relative_location",
+    "RootRelativeLocationCodegen",
     "RootRelativeLocationParser",
-    "RootRelativeLocationLiteralParser",
-    "resolve_using_database",
 ]
 
 
 from dataclasses import dataclass
+from functools import partial
 
-from beet import Context
-from bolt import AstValue
+from beet import Context, Generator
+from bolt import Accumulator, InterpolationParser, Runtime
 from tokenstream import TokenStream, set_location
 from mecha import (
+    AlternativeParser,
     AstResourceLocation,
     CommentDisambiguation,
     Mecha,
     Parser,
+    Visitor,
+    rule,
 )
 
 
@@ -25,22 +32,60 @@ PATTERN = r"#?~/[0-9a-z_./-]+"
 
 
 def beet_default(ctx: Context):
+    ctx.require(root_relative_location)
+
+
+def root_relative_location(ctx: Context):
     mc = ctx.inject(Mecha)
+    runtime = ctx.inject(Runtime)
+
     parsers = mc.spec.parsers
-    parsers["resource_location_or_tag"] = CommentDisambiguation(
-        RootRelativeLocationParser(parser=parsers["resource_location_or_tag"], ctx=ctx)
+
+    parsers["resource_location_or_tag"] = AlternativeParser(
+        [
+            InterpolationParser("resource_location"),
+            CommentDisambiguation(
+                RootRelativeLocationParser(parser=parsers["resource_location_or_tag"])
+            ),
+        ]
     )
-    parsers["bolt:literal"] = RootRelativeLocationLiteralParser(
-        parser=parsers["bolt:literal"], ctx=ctx
+    parsers["bolt:literal"] = RootRelativeLocationParser(
+        parser=parsers["bolt:literal"], literal=True
     )
+    parsers["bolt:import"] = RootRelativeLocationParser(
+        parsers["bolt:import"], generate=ctx.generate
+    )
+
+    runtime.helpers["resolve_root_resource_location"] = partial(
+        resolve_root_resource_location, ctx.generate
+    )
+
+    runtime.modules.codegen.extend(RootRelativeLocationCodegen())
+
+
+@dataclass(frozen=True)
+class AstRootResourceLocation(AstResourceLocation):
+    """Ast root-relative resource location node."""
+
+    literal: bool = False
 
 
 @dataclass
 class RootRelativeLocationParser:
-    """Parser that resolves root-relative resource locations."""
+    """
+    Parser that resolves root-relative resource locations.
+
+    `generate`: A `Generator` object. If provided, root-relative resource
+    locations are resolved during parsing.
+
+    `literal`: A flag. If true, the parsed `AstRootResourceLocation` node
+    will be treated like a literal value during codegen, being resolved to a
+    plain string instead of a `AstResourceLocation` node.
+    """
 
     parser: Parser
-    ctx: Context
+    generate: Generator | None = None
+    literal: bool = False
 
     def __call__(self, stream: TokenStream) -> AstResourceLocation:
         with stream.syntax(root_resource_location=PATTERN):
@@ -50,29 +95,43 @@ class RootRelativeLocationParser:
                 return self.parser(stream)
 
             is_tag = token.value.startswith("#")
-            value = token.value[3:] if is_tag else token.value[2:]
-            full_path = self.ctx.generate.path(value)
-            namespace, _, path = full_path.rpartition(":")
+            path = token.value[3:] if is_tag else token.value[2:]
 
-            node = AstResourceLocation(is_tag=is_tag, namespace=namespace, path=path)
+            if self.generate:
+                full_path = self.generate.path(path)
+                namespace, _, path = full_path.rpartition(":")
+                node = AstResourceLocation(
+                    is_tag=is_tag, namespace=namespace, path=path
+                )
+            else:
+                node = AstRootResourceLocation(
+                    is_tag=is_tag, path=path, literal=self.literal
+                )
+
             return set_location(node, token)
 
 
 @dataclass
-class RootRelativeLocationLiteralParser:
-    parser: Parser
-    ctx: Context
+class RootRelativeLocationCodegen(Visitor):
+    @rule(AstRootResourceLocation)
+    def root_location(self, node: AstRootResourceLocation, acc: Accumulator):
+        result = acc.make_variable()
+        value = acc.helper(
+            "resolve_root_resource_location", f"{node.path!r}", node.is_tag
+        )
+        acc.statement(f"{result} = {value}", lineno=node)
 
-    def __call__(self, stream: TokenStream) -> AstValue:
-        with stream.syntax(root_resource_location=PATTERN):
-            token = stream.get("root_resource_location")
+        if not node.literal:
+            rhs = acc.helper(
+                "interpolate_resource_location", result, acc.make_ref(node)
+            )
+            acc.statement(f"{result} = {rhs}")
 
-            if token is None:
-                return self.parser(stream)
+        return [result]
 
-            is_tag = token.value.startswith("#")
-            stripped = token.value[3:] if is_tag else token.value[2:]
-            value = "#" * is_tag + self.ctx.generate.path(stripped)
 
-            node = AstValue(value=value)
-            return set_location(node, token)
+def resolve_root_resource_location(
+    gen: Generator, path: str, is_tag: bool = False
+) -> str:
+    path = gen.path(path)
+    return "#" + path if is_tag else path
